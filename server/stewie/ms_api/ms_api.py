9376115aa0e958f1b94520337fdb9055f3fdd375
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-23 23:18:11 +0100

    Rename folders

--- server/stewie/ms_api/ms_api.py
+++ server/stewie/ms_api/ms_api.py
@@ -4,8 +4,8 @@ import requests
 
 headers  = {'Ocp-Apim-Subscription-Key': MICROSOFT_KEY, "Content-Type": "application/octet-stream" }
 
-def get_image_emotion(image_url):
-    image_path = "images/emotion_1.jpg"
+def get_image_emotion(image_data):
+    #image_path = "images/emotion_1.jpg"
     image_data = open(image_path, "rb").read()
     requests.post(emotion_recognition_url, headers=headers, data=image_data)
     response.raise_for_status()
