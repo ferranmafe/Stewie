40525325d7c6e505f5df7f1771e3a0e522de89f0
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-24 01:03:32 +0100

    Update GITIGNORE and get request

--- server/stewie/ms_api/ms_api.py
+++ server/stewie/ms_api/ms_api.py
@@ -1,13 +1,17 @@
 
-from app.stwie_mind import * 
+from server.stewie import * 
 import requests
 
 headers  = {'Ocp-Apim-Subscription-Key': MICROSOFT_KEY, "Content-Type": "application/octet-stream" }
 
-def get_image_emotion(image_data):
-    #image_path = "images/emotion_1.jpg"
+def get_image_emotion(image_path):
     image_data = open(image_path, "rb").read()
-    requests.post(emotion_recognition_url, headers=headers, data=image_data)
+    response = requests.post(MICROSOFT_URL, headers=headers, data=image_data)
     response.raise_for_status()
     analysis = response.json()
+    print(analysis)
     return analysis
+
+
+if __name__ == '__main__':
+    get_image_emotion()
