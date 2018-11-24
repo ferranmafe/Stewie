c4df9be8e478064b2be690b2262d281907d57392
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-24 13:10:41 +0100

    Add spotify call (playlist getting tracks and get info tracks), ms api is working, and update enviorment variables

--- server/stewie/spotify_api/spotify.py
+++ server/stewie/spotify_api/spotify.py
@@ -1,28 +1,23 @@
-from server.utils import log
-import requests
 
-def get_list_playlists(token):
+import requests
+import json
+from pprint import pprint
+
+def get_list_playlists_call(token):
     url = 'https://api.spotify.com/v1/me/playlists'
     headers  = { 'Authorization': 'Bearer ' + token }
     response = requests.post(url, headers=headers)
 
-    result = []
-    for playlist in response.items:
-        result.insert({
-            'id': playlist.id,
-            'images': playlist.images,
-            'name': playlist.name
-        })
-    return result
+    return response.text
 
-def get_tracks_from_playlist(id, token):
+def get_tracks_from_playlist_call(id, token):
     url = 'https://api.spotify.com/v1/playlists/'+ id + '/tracks'
-    headers  = { 'Authorization': 'Bearer ' + token }
-    response = requests.get(url, headers=headers)
-    return response
+    headers  = { 'Authorization': 'Bearer ' + token, 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    response = requests.get(url, headers=headers )
+    return response.text
 
-def get_track_info(id,token):
-    url = 'https://api.spotify.com/v1/audio-features/' + id
-    headers  = { 'Authorization': 'Bearer ' + token }
-    response = requests.get(url, headers=headers)
-    return response
+def get_track_info_call(id,token):
+    url = 'https://api.spotify.com/v1/audio-features?ids=' + id
+    headers  = { 'Authorization': 'Bearer ' + token, 'cache-control': "no-cache", 'Content-type': 'application/json', 'Accept': 'application/json' }
+    response = requests.get(url,data='', headers=headers)
+    return response.text
